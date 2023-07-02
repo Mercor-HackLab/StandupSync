a4e4b8c6db9c6b3ba00b06f5ffd3681ce03c164c
Aparna Goyal <aparnagoyal.0003@gmail.com>
2023-07-02 14:37:18 +0530

    exception handling

--- standup_analyzer.py
+++ standup_analyzer.py
@@ -13,33 +13,39 @@ def analyze_weekly_data():
     start_datetime = get_current_datetime_in_local_timezone() - dt.timedelta(days=7)
     end_datetime = get_current_datetime_in_local_timezone()
 
-    events_result = service.events().list(
-        calendarId=CALENDAR_ID,
-        timeMin=start_datetime.isoformat(),
-        timeMax=end_datetime.isoformat(),
-        maxResults=2500,  # Adjust the number of events to retrieve
-        singleEvents=True,
-        orderBy='startTime'
-    ).execute()
+    try:
+        events_result = service.events().list(
+            calendarId=CALENDAR_ID,
+            timeMin=start_datetime.isoformat(),
+            timeMax=end_datetime.isoformat(),
+            maxResults=2500,  
+            singleEvents=True,
+            orderBy='startTime'
+        ).execute()
 
-    events = events_result.get('items', [])
+        events = events_result.get('items', [])
 
-    if not events:
-        print('No events found for the past week.')
-        return
+        if not events:
+            print('No events found for the past week.')
+            return
 
-    data = ''
-    for event in events:
-        summary = event.get('summary')
-        if(summary == 'DSM'):
-            description = event.get('description')
-            start_date = event.get('start').get('dateTime')
-            data += f"Event Start Time: {start_date}\n"
-            data += f"Event Description: {description}\n"
+        data = ''
+        for event in events:
+            summary = event.get('summary')
+            if(summary == 'DSM'):
+                description = event.get('description')
+                start_date = event.get('start').get('dateTime')
+                data += f"Event Start Time: {start_date}\n"
+                data += f"Event Description: {description}\n"
 
-    prompt = f"Who is the top Performer based on the {data} ."
-    gpt_result = askGPT(prompt)
-    print("Top Performer: ", gpt_result)
+        prompt = f"What are the areas of improvement for employees based on the following DSM events:\n{data}\n"
+        gpt_result = askGPT(prompt)
+        print("Areas of Improvement for Employees:")
+        print(gpt_result)
+    
+    except HttpError as err:
+        logging.error('An error occurred while fetching events from Google Calendar: %s', err)
+
 
 # if __name__ == "__main__":
 #     analyze_weekly_data()
