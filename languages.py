179e964f3ae9d5afa8a8f8511d11669873af5264
rishusingh022 <rishusingh022@gmail.com>
2023-07-02 12:24:40 +0530

    feat: add multi language functionality

---
+++ languages.py
@@ -0,0 +1,13 @@
+languages = {
+    'Bengali (India)': 'bn-IN',
+    'English (India)': 'en-IN',
+    'Gujarati (India)': 'gu-IN',
+    'Hindi (India)': 'hi-IN',
+    'Kannada (India)': 'kn-IN',
+    'Malayalam (India)': 'ml-IN',
+    'Marathi (India)': 'mr-IN',
+    'Tamil (India)': 'ta-IN',
+    'Telugu (India)': 'te-IN',
+    'English (United States)': 'en-US',
+    'Spanish (United States)': 'es-US'
+}
